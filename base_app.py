0faec8adc319069565fd45dcd528725945516f02
JasonAlexanderDunbar <jasonalexanderdunbar@gmail.com>
2021-06-22 22:36:38 +0200

    more updates

--- base_app.py
+++ base_app.py
@@ -33,7 +33,7 @@ news_vectorizer = open("resources/vect.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
-raw = pd.read_csv("train.csv")
+raw = pd.read_csv("resources/train.csv")
 
 # The main function where we will build the actual app
 def main():
